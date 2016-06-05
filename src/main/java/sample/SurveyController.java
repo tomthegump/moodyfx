de026c16fdd37f90b4778c99bb7649040b3718cc
Ingo Schwarz <schwarz-ingo@web.de>
2016-06-05 13:20:23 +0200

    Improve survey layout

--- src/main/java/sample/SurveyController.java
+++ src/main/java/sample/SurveyController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import sample.data.Survey;
 
 import java.net.URL;
@@ -58,6 +60,7 @@ public class SurveyController implements Initializable {
         for (int i = 0; i < 5; i++) {
             String resourceName = iconType.getResourceName(i);
             ImageView imageView = new ImageView(getClass().getResource(resourceName).toString());
+            HBox.setHgrow(imageView, Priority.ALWAYS);
             moodSelectionArea.getChildren().add(imageView);
             imageView.setOnMouseClicked(new VotingListener(i));
         }
@@ -68,11 +71,13 @@ public class SurveyController implements Initializable {
 
         String resourceName1 = iconType.getResourceName(1);
         ImageView imageView1 = new ImageView(getClass().getResource(resourceName1).toString());
+        HBox.setHgrow(imageView1, Priority.ALWAYS);
         moodSelectionArea.getChildren().add(imageView1);
         imageView1.setOnMouseClicked(new VotingListener(1));
 
         String resourceName3 = iconType.getResourceName(3);
         ImageView imageView3 = new ImageView(getClass().getResource(resourceName3).toString());
+        HBox.setHgrow(imageView3, Priority.ALWAYS);
         moodSelectionArea.getChildren().add(imageView3);
         imageView3.setOnMouseClicked(new VotingListener(3));
     }
@@ -83,8 +88,18 @@ public class SurveyController implements Initializable {
         for (int i = 1; i < 4; i++) {
             String resourceName = iconType.getResourceName(i);
             ImageView imageView = new ImageView(getClass().getResource(resourceName).toString());
-            moodSelectionArea.getChildren().add(imageView);
+            imageView.setPreserveRatio(true);
+            imageView.maxWidth(Double.MAX_VALUE);
+            imageView.maxHeight(Double.MAX_VALUE);
             imageView.setOnMouseClicked(new VotingListener(i));
+            HBox.setHgrow(imageView, Priority.ALWAYS);
+            moodSelectionArea.getChildren().add(imageView);
+
+            if (i < 3) {
+                Region spacer = new Region();
+//                HBox.setHgrow(spacer, Priority.ALWAYS);
+                moodSelectionArea.getChildren().add(spacer);
+            }
         }
     }
 
